239d3a9948e8dd7f60dbd126d285aab6f7609419
Nicolas Martinez V <nmartinezv@icloud.com>
2026-07-19 15:41:50 -0400

    feat(wzterm): base file

---
+++ .wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("Hack Nerd Font")
+config.font_size = 14
+config.color_scheme = "Tokyo Night Storm"
+
+config.window_background_opacity = 1
+config.macos_window_background_blur = 10
+
+config.tab_and_split_indices_are_zero_based = true
+config.tab_bar_at_bottom = true
+
+return config
