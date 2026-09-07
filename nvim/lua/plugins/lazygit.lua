074bfc8ac572bd1c48966b5c4278fb775c9513d1
Nicolas Martinez <nmartinezv@icloud.com>
2026-09-07 19:26:27 -0300

    fix(nvim): remove bad Checker guard in lazygit — plugin has no Lua module

--- nvim/lua/plugins/lazygit.lua
+++ nvim/lua/plugins/lazygit.lua
@@ -5,18 +5,9 @@ M.plugin = function()
     { src = 'https://github.com/kdheepak/lazygit.nvim' },
   })
 
-  if not Checker.check('lazygit') then
-    return
-  end
-
   KM.map({
     motion = '<leader>glg',
-    cmd = function()
-      if not package.loaded['lazygit'] then
-        M.plugin()
-      end
-      vim.cmd('LazyGit')
-    end,
+    cmd = '<cmd>LazyGit<CR>',
     opts = { desc = 'Open Lazygit' },
   })
 end
