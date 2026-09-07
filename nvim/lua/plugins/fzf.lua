a77a0db6d6f251dd74c8b536b5bd1da103dd0d6d
Nicolas Martinez <nmartinezv@icloud.com>
2026-09-07 19:24:30 -0300

    fix(nvim): exclude .git contents from fzf-lua files and grep

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -68,10 +68,10 @@ M.plugin = function()
 
   local opts = {
     files = {
-      fd_opts = '--color=never --type f --hidden --follow --exclude .git',
+      fd_opts = '--color=never --type f --hidden --follow --exclude .git --exclude .git/**',
     },
     grep = {
-      rg_opts = '--column --line-number --hidden -g "!.git"',
+      rg_opts = '--column --line-number --hidden --glob=!.git --glob=!.git/**',
     },
   }
 
