a1d6457b8c7a9ba256b20baeaccf4157f7623e0c
Nix Auto Sync <nicolas@outer-heaven>
2026-09-08 14:14:16 -0300

    chore: sync local dotfiles changes from host

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,14 +1,30 @@
 local M = {}
 
 local kind_icons = {
-  Text          = '󰉿', Method      = '󰆧', Function    = '󰊕',
-  Constructor   = '',  Field       = '󰜢', Variable    = '󰀫',
-  Class         = '󰠱', Interface   = '',  Module      = '',
-  Property      = '󰜢', Unit        = '󰑭', Value       = '󰎠',
-  Enum          = '',  Keyword     = '󰌋', Snippet     = '',
-  Color         = '󰏘', File        = '󰈙', Reference   = '󰈇',
-  Folder        = '󰉋', EnumMember  = '',  Constant    = '󰏿',
-  Struct        = '󰙅', Event       = '',  Operator    = '󰆕',
+  Text = '󰉿',
+  Method = '󰆧',
+  Function = '󰊕',
+  Constructor = '',
+  Field = '󰜢',
+  Variable = '󰀫',
+  Class = '󰠱',
+  Interface = '',
+  Module = '',
+  Property = '󰜢',
+  Unit = '󰑭',
+  Value = '󰎠',
+  Enum = '',
+  Keyword = '󰌋',
+  Snippet = '',
+  Color = '󰏘',
+  File = '󰈙',
+  Reference = '󰈇',
+  Folder = '󰉋',
+  EnumMember = '',
+  Constant = '󰏿',
+  Struct = '󰙅',
+  Event = '',
+  Operator = '󰆕',
   TypeParameter = '',
 }
 
@@ -130,9 +146,9 @@ local lsp_autocompletion_fn = function(args)
   vim.lsp.completion.enable(true, client.id, args.buf, {
     autotrigger = true,
     convert = function(item)
-      local kind_num  = item.kind or 1
+      local kind_num = item.kind or 1
       local kind_name = vim.lsp.protocol.CompletionItemKind[kind_num] or 'Text'
-      local icon      = kind_icons[kind_name] or '?'
+      local icon = kind_icons[kind_name] or '?'
       return {
         abbr = item.label,
         kind = icon .. ' ' .. kind_name,
@@ -148,25 +164,33 @@ local lsp_autocompletion_fn = function(args)
     {
       mode = 'i',
       motion = '<CR>',
-      cmd = function() return vim.fn.pumvisible() == 1 and '<C-y>' or '<CR>' end,
+      cmd = function()
+        return vim.fn.pumvisible() == 1 and '<C-y>' or '<CR>'
+      end,
       opts = { buffer = buf, expr = true, desc = 'Completion: confirmar' },
     },
     {
       mode = 'i',
       motion = '<Tab>',
-      cmd = function() return vim.fn.pumvisible() == 1 and '<C-n>' or '<Tab>' end,
+      cmd = function()
+        return vim.fn.pumvisible() == 1 and '<C-n>' or '<Tab>'
+      end,
       opts = { buffer = buf, expr = true, desc = 'Completion: siguiente' },
     },
     {
       mode = 'i',
       motion = '<S-Tab>',
-      cmd = function() return vim.fn.pumvisible() == 1 and '<C-p>' or '<S-Tab>' end,
+      cmd = function()
+        return vim.fn.pumvisible() == 1 and '<C-p>' or '<S-Tab>'
+      end,
       opts = { buffer = buf, expr = true, desc = 'Completion: anterior' },
     },
     {
       mode = 'i',
       motion = '<C-Space>',
-      cmd = function() vim.lsp.completion.trigger() end,
+      cmd = function()
+        vim.lsp.completion.trigger()
+      end,
       opts = { buffer = buf, desc = 'Completion: trigger manual' },
     },
     {
